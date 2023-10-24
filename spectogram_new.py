edad08f00470b23b7a1aa00e4c4a2c0172efaaea
Santiago Campo <id00706137@usal.es>
2023-10-24 12:21:55 +0200

    Adding save result plot directory

--- spectogram_new.py
+++ spectogram_new.py
@@ -1,11 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import os
 import librosa
 import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
 import argparse
-import os
 
-def plot_spectrogram(y, sr, hop_length, n_fft):
+def plot_spectrogram(y, sr, hop_length, n_fft, result_folder=None, filename=None):
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length)), ref=np.max)
 
     plt.figure(figsize=(10, 4))
@@ -13,13 +13,25 @@ def plot_spectrogram(y, sr, hop_length, n_fft):
     plt.colorbar(format='%+2.0f dB')
     plt.title('Spectrogram')
     plt.tight_layout()
-    plt.show()
+    
+    # Save the figure if result_folder is provided
+    if result_folder:
+        if not os.path.exists(result_folder):
+            os.makedirs(result_folder)
+        plt.show()
+        
+        save_path = os.path.join(result_folder, f"{filename}_spectrogram.png")
+        plt.savefig(save_path)
+        print(f"Spectrogram saved to {save_path}")
+    
+    else:
+        plt.show()
 
-def check_and_process(file_folder):
+def check_and_process(file_folder, result_folder=None):
     if os.path.isfile(file_folder):
         try:
             y, sr = librosa.load(file_folder, sr=None)
-            plot_spectrogram(y, sr, hop_length=512, n_fft=2048)
+            plot_spectrogram(y, sr, hop_length=512, n_fft=2048, result_folder=result_folder, filename=os.path.basename(file_folder).split('.')[0])
         except Exception as e:
             print(f"Error processing file {file_folder}: {e}")
 
@@ -30,7 +42,7 @@ def check_and_process(file_folder):
             print(f"Plotting spectrogram for {file}")
             try:
                 y, sr = librosa.load(os.path.join(file_folder, file), sr=None)
-                plot_spectrogram(y, sr, hop_length=512, n_fft=2048)
+                plot_spectrogram(y, sr, hop_length=512, n_fft=2048, result_folder=result_folder, filename=file.split('.')[0])
             except Exception as e:
                 print(f"Error processing file {file}: {e}")
     else:
@@ -41,8 +53,9 @@ def argument_parser():
         description="Plot spectrogram for a given audio file or folder",
         )
     parser.add_argument('-f', '--file_folder', help='Path to audio file or folder', required=True)
+    parser.add_argument('-r', '--folder_result', help='Path to save the spectrogram result', default=None)
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = argument_parser()
-    check_and_process(args.file_folder)
+    check_and_process(args.file_folder, result_folder=args.folder_result)

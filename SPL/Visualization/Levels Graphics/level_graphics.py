05a18f3b06ad540a6eca5835aeda6180efd9138d
Santiago Campo <id00706137@usal.es>
2023-11-06 10:54:10 +0100

    Adding the level_graphics program to perform the same visualization as in Sonometer Analisys

--- SPL/Visualization/Levels Graphics/level_graphics.py
+++ SPL/Visualization/Levels Graphics/level_graphics.py
@@ -4,28 +4,43 @@ import matplotlib.dates as mdates
 import seaborn as sns
 import numpy as np
 import argparse
+
+def plt_name(plotname: str):
+    if "_spl" or "_spl_oct_" in plotname:
+        plotname = plotname.split("_spl")[0]
+    return plotname
  
 def leq(levels):
-    # Filter out NaN values and ensure all values are floats
     levels = np.array(levels.dropna(), dtype=float)
     if len(levels) == 0:
         return np.nan
     return 10 * np.log10(np.mean(np.power(10, levels / 10)))
 
 def plot_heatmap(df, values_column: str, agg_func: callable, plotname: str):
+    plotname = plt_name(plotname)
+
     df['day'] = df['date'].dt.date
     df['hour'] = df['date'].dt.hour
     leq_day_hour = pd.pivot_table(df, values=values_column, index=['day'], columns=['hour'], aggfunc=agg_func).round(1)
     plt.figure(figsize=(20, 5))
-    sns.heatmap(leq_day_hour, cmap='viridis', annot=True, fmt='.1f')
+
+    plt.title(f"{plotname.replace('_', ' ')}")
+    
+    sns.heatmap(leq_day_hour, cmap='YlOrRd', annot=True, fmt='.1f')  # colormap to 'Reds'
     plt.xlabel('Hour')
     plt.ylabel('Day')
     plt.tight_layout()
+    
     plt.savefig(f'{plotname}_heatmap.png', dpi=150)
     leq_day_hour.to_excel(f'{plotname}_heatmap_table_day_hour.xlsx')
+    
     plt.close()
 
+
+
 def make_timeplot(df, columns_dict: dict, agg_period: int, plotname: str):
+    plotname = plt_name(plotname)
+
     for col in columns_dict.values():
         df[col] = pd.to_numeric(df[col], errors='coerce')
     df.dropna(subset=[columns_dict['LAEQ_COLUMN']], inplace=True)
@@ -37,27 +52,37 @@ def make_timeplot(df, columns_dict: dict, agg_period: int, plotname: str):
     
     L90 = df[columns_dict['LAEQ_COLUMN']].resample(f'{agg_period}s').quantile(0.1)
     L10 = df[columns_dict['LAEQ_COLUMN']].resample(f'{agg_period}s').quantile(0.9)
+    # colour
+    L5 = df[columns_dict['LAEQ_COLUMN']].resample(f'{agg_period}s').quantile(0.95)
+    L1 = df[columns_dict['LAEQ_COLUMN']].resample(f'{agg_period}s').quantile(0.99)
+    L50 = df[columns_dict['LAEQ_COLUMN']].resample(f'{agg_period}s').quantile(0.50)
     
     fig, ax = plt.subplots(figsize=(20, 10))
     ax.set_facecolor("white")
+    plt.title(f"{plotname.replace('_', ' ')} | {agg_period}s")
     
     x = leq_agg.index
     ax.plot(x, leq_agg.values, linewidth=3, color='red')
-    ax.plot(x, lmax_agg.values, linewidth=0.8, color='green')
-    ax.plot(x, lmin_agg.values, linewidth=0.8, color='blue')
+    ax.plot(x, lmax_agg.values, linewidth=0.8, color='#FF99FF')
+    ax.plot(x, lmin_agg.values, linewidth=0.8, color='#92D050')
     ax.plot(x, L90, linewidth=0.8, color='#525252')
     ax.plot(x, L10, linewidth=0.8, color='#8497B0')
+    ax.plot(x, L5, linewidth=0.8, color='#FF0000')
+    ax.plot(x, L1, linewidth=0.8, color='#FF0000')
+    ax.plot(x, L50, linewidth=0.8, color='#000000')
 
-    hours = mdates.HourLocator(interval=1)
+    hours = mdates.HourLocator(interval=2)
     h_fmt = mdates.DateFormatter('%H:%M')
     ax.xaxis.set_major_locator(hours)
     ax.xaxis.set_major_formatter(h_fmt)
 
+    ax.set_xlim(df.index.min(), df.index.max())
+
     plt.ylabel('dB(A)')
     plt.xlabel('Hour')
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
     plt.ylim([30, 120])
-    plt.legend(['Leq', 'Lmax', 'Lmin', 'L90', 'L10'], bbox_to_anchor=(1.1, 1.05))
+    plt.legend(['Leq', 'Lmax', 'Lmin', 'L90', 'L10', 'L5', 'L1', 'L50'], bbox_to_anchor=(1, 1), loc='upper left')
     
     plt.tight_layout()
     plt.savefig(f'{plotname}_{agg_period}s_timeplot.png', dpi=150)
@@ -68,6 +93,8 @@ def arg_parser():
     parser.add_argument('-f', '--csv-file', type=str, required=True, help='CSV file with AudioMoth data')
     parser.add_argument('-a', '--agg_period', type=int, required=False, default=3600, help='Aggregation period in seconds')
     return parser.parse_args()
+
+
 
 def main():
     args = arg_parser()
@@ -82,16 +109,17 @@ def main():
         'LAMAX_COLUMN': 'LAmax',
         'LAMIN_COLUMN': 'LAmin'
     }
-    
-    # Ensure that the LA column is a float and not string
+
     df[columns_dict['LAEQ_COLUMN']] = df[columns_dict['LAEQ_COLUMN']].apply(lambda x: float(str(x).replace(',', '.')))
 
-    # Generate heatmap
-    plotname = csv_file.split('.')[0]  # Use the CSV filename as the plotname prefix
+    # heatmap
+    plotname = csv_file.split('.')[0]  
     plot_heatmap(df, 'LA', leq, plotname)
 
-    # Generate timeplot
+    # timeplot
     make_timeplot(df, columns_dict, agg_period, plotname)
+
+
 
 if __name__ == "__main__":
     main()

83a0f08342d1a1cc0209ad44cd4d63a70d7d5d98
Santiago Campo Jurado <id00706137@usal.es>
2023-11-27 09:45:42 +0100

    Update logging formatters in logging_config.py

--- Sonometer/test/logging_config.py
+++ Sonometer/test/logging_config.py
@@ -9,15 +9,14 @@ def setup_logging(log_file='sonometer.log', level=logging.DEBUG):
         Returns:
             logger: logging.Logger object
     """
-    
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
 
     file_handler = logging.FileHandler(log_file)
     console_handler = logging.StreamHandler()
 
-    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(name)s - %(levelname)s - %(message)s')
+    console_formatter = logging.Formatter('%(filename)s - %(name)s - %(levelname)s - %(message)s')
 
     file_handler.setFormatter(file_formatter)
     console_handler.setFormatter(console_formatter)

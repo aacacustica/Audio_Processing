bf34d23b82eaf1b901e233c073abe75e22fd5471
Santiago Campo <id00706137@usal.es>
2023-12-04 15:22:51 +0100

    Refactor get_metadata function to handle both files and folders

--- Check_audio_files/main.py
+++ Check_audio_files/main.py
@@ -1,10 +1,14 @@
 from process_metadata import *
 
 def main():
-    file_path = "/home/santi/Documents/AAC/audios/20231019_173510.WAV"
-    folder_path = "/home/santi/Documents/AAC/audios/"
-    metadata = get_metadata(file_path)
-    print(metadata)
+    # file_path = "/home/santi/Documents/AAC/audios/20231019_173510.WAV"
+    # folder_path = "/home/santi/Documents/AAC/audios/"
+    file_path = input("Enter file path: ")
+    try: 
+        metadata = get_metadata(file_path)
+        print(metadata)
+    except Exception as e:
+        print("Error: ", e)
 
 if __name__ == "__main__":
     main()
--- Check_audio_files/process_metadata.py
+++ Check_audio_files/process_metadata.py
@@ -1,13 +1,27 @@
 from pydub.utils import mediainfo
-import json
+import os
+import tqdm
 
-def get_metadata(file_path: str):
-    """Get metadata from audio file
-    Args:
-        file_path (str): path to audio file
-    Returns:
-        metadata_json (json): metadata from audio file"""
+def get_metadata(path: str):
+    if os.path.isdir(path):
+        metadata_dict = {}
     
-    metadata = mediainfo(file_path)
-    metadata_json = json.dumps(metadata, indent=4)
-    return metadata_json
+        # for file in os.listdir(path):
+        for file in tqdm.tqdm(os.listdir(path)):
+            full_path = os.path.join(path, file)
+            if os.path.isfile(full_path):
+    
+                try:
+                    metadata = mediainfo(full_path)
+                    metadata_dict[file] = metadata
+                except Exception as e:
+                    print(f"Error processing {file}: {e}")
+    
+        return metadata_dict
+    
+    else:
+        try:
+            metadata = mediainfo(path)
+            return metadata
+        except Exception as e:
+            raise Exception(f"Error processing file: {e}")

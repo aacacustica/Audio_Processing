bbcb247d900e18bba5253c5ce1b758b306b6d460
Santiago Campo <id00706137@usal.es>
2023-12-05 16:55:43 +0100

    Remove unnecessary code and add metadata storage

--- Integrity_Audio_Files/main.py
+++ Integrity_Audio_Files/main.py
@@ -5,18 +5,13 @@ def main():
     logger = setup_logging()
 
     # path = "/home/santi/Documents/AAC/audios/20231019_173510.WAV"
-    # path = "/home/santi/Documents/AAC/audios/"
     path = "/home/santi/Documents/AAC/audios/AudioMoths"
-    # path = input("Enter path for the file or folder: ")
     
     try: 
         metadata = get_metadata(path, logger)
-        # for key, value in metadata.items():
-        #     logger.debug(f"{key}: {value}")
 
     except Exception as e:
         logger.error("Error: ", e)
-
 
 if __name__ == "__main__":
     main()
--- Integrity_Audio_Files/process_metadata.py
+++ Integrity_Audio_Files/process_metadata.py
@@ -18,6 +18,9 @@ def get_metadata(path: str, logger):
     # if path is a file or a folder
     if os.path.isdir(path):
         metadata_dict = {}
+
+        # STORE METADTA
+
     
         # for files in folder
         for file in tqdm.tqdm(os.listdir(path)):
@@ -94,37 +97,4 @@ def get_metadata(path: str, logger):
         
         except Exception as e:
             raise Exception(f"Error processing file: {e}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def argument_parser():
-#     """Argument parser to set the parameters of the script to 
-#     check the integrity of audio files."""
-
-#     parser = argparse.ArgumentParser(description="Process metadata of audio files.")
-#     parser.add_argument("-p", "--path", type=str, help="Path to the file or folder.", required=False)
-#     parser.add_argument("-o", "--output", type=str, help="Path to the output file.", required=False)
-
-#     parser.add_argument("-sr", "--samplerate", type=int, help="Sample rate of the audio files.", required=False, default=16000)
-#     parser.add_argument("-c", "--channels", type=int, help="Number of channels of the audio files.", required=False, default=1)
-#     parser.add_argument("-g", "--gain", type=int, help="Gain of the audio files.", required=False, choices=["Low", "Med", "High"], default="Med")
-#     parser.add_argument("-sd", "--sleep-duration", type=int, help="Sleep duration of the audio files in seconds.", required=False, default=5)
-#     parser.add_argument("-d", "--duration", type=int, help="Duration of the audio files in seconds.", required=False, default=55)
-
-
-#     return parser.parse_args()
+        

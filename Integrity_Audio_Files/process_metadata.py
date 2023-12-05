495dbc8d261cd3c9898fa8362187d3b2be194c3f
Santiago Campo <id00706137@usal.es>
2023-12-05 20:22:45 +0100

    executing exiftool instead it's clearer

--- Integrity_Audio_Files/process_metadata.py
+++ Integrity_Audio_Files/process_metadata.py
@@ -3,75 +3,89 @@ import os
 import tqdm
 # import argparse
 from test_integrity import *
+import subprocess
 
 def get_metadata(path: str, logger):
-    """Returns a dictionary with the metadata of the file or files in the path.
-    Args: 
-        path (str): Path to the file or folder.
-    Returns:
-        integrity_dict (dict): Dictionary with the metadata of the file or files.
-    """
+    """Returns a dictionary with the metadata of the file or files in the path."""
     integrity_dict = {}
-    
-    # if path exists
+
     if not os.path.exists(path):
         raise Exception("Path does not exist.")
-    
-    # if path is a file or a folder
+
+    # if it is a dir
     if os.path.isdir(path):
-        # for files in folder
         for file in tqdm.tqdm(os.listdir(path)):
+            # get the full path
             full_path = os.path.join(path, file)
-            if os.path.isfile(full_path):    
+            # check if it is a file
+            if os.path.isfile(full_path):
                 try:
-                    # getting metadata
-                    metadata = mediainfo(full_path)
+                    # setting ExifTool
+                    result = subprocess.run(['exiftool', full_path], stdout=subprocess.PIPE, text=True)
+                    metadata = result.stdout
+        
+                    parsed_metadata = {}
+                    for line in metadata.split('\n'):
+                        if ': ' in line:
+                            key, value = line.split(': ', 1)
+                            parsed_metadata[key.strip()] = value.strip()
 
-                    # testing integrity
-                    # [1] calibration
-                    filename, audiomoth_name, file_calibration = test_name_calibration(metadata, logger)
-                    # [2] test channels
-                    channels = test_channels(metadata, logger)
-                    # [3] test sample rate
-                    sample_rate = test_sample_rate(metadata, logger)
-                    # [4] test time zone
-                    time_zone_metadata = test_time_zone(metadata, logger)
-                    # [5] test battery status
-                    battery_voltage = test_batery_status(metadata, logger)
-                    # [6] test gain
-                    gain = test_gain(metadata, logger)
-                    # [7] test recording duration
-                    duration = test_recording_duration(metadata, logger)
-                    # [8] test temperature
-                    temperature = test_temperature(metadata, logger)
-                    # [9] test timestamp
-                    timestamp = test_timestamp(metadata, logger)
-
-                    # save the metadata in a dictionary
-                    integrity = {
-                        "filename": filename,
-                        "audiomoth_name": audiomoth_name,
-                        "file_calibration": file_calibration,
-                        "channels": channels,
-                        "sample_rate": sample_rate,
-                        "time_zone_metadata": time_zone_metadata,
-                        "battery_voltage": battery_voltage,
-                        "gain": gain,
-                        "duration": duration,
-                        "temperature": temperature,
-                        "timestamp": timestamp
-                    }
-                    # save the dictionary
-                    integrity_dict[file] = integrity
+                    print(parsed_metadata)
                 
                 except Exception as e:
                     print(f"Error processing file {file}: {e}")
+                #     # getting metadata
+                #     metadata = mediainfo(full_path)
+                #     # for k, v in metadata.items():
+                #     #     print(f"{k}: {v}")
+
+                #     # testing integrity
+                #     # [1] calibration
+                #     filename, audiomoth_name, file_calibration = test_name_calibration(metadata, logger)
+                #     # [2] test channels
+                #     channels = test_channels(metadata, logger)
+                #     # [3] test sample rate
+                #     sample_rate = test_sample_rate(metadata, logger)
+                #     # [4] test time zone
+                #     time_zone_metadata = test_time_zone(metadata, logger)
+                #     # [5] test battery status
+                #     battery_voltage = test_batery_status(metadata, logger)
+                #     # [6] test gain
+                #     gain = test_gain(metadata, logger)
+                #     # [7] test recording duration
+                #     duration = test_recording_duration(metadata, logger)
+                #     # [8] test temperature
+                #     temperature = test_temperature(metadata, logger)
+                #     # [9] test timestamp
+                #     timestamp = test_timestamp(metadata, logger)
+
+                #     # save the metadata in a dictionary
+                #     integrity = {
+                #         "filename": filename,
+                #         "audiomoth_name": audiomoth_name,
+                #         "file_calibration": file_calibration,
+                #         "channels": channels,
+                #         "sample_rate": sample_rate,
+                #         "time_zone_metadata": time_zone_metadata,
+                #         "battery_voltage": battery_voltage,
+                #         "gain": gain,
+                #         "duration": duration,
+                #         "temperature": temperature,
+                #         "timestamp": timestamp
+                #     }
+                #     # save the dictionary
+                #     integrity_dict[file] = integrity
+                
+                # except Exception as e:
+                #     print(f"Error processing file {file}: {e}")
         return integrity_dict
     
     else:
         try:
             # metadata
             metadata = mediainfo(path)
+            # for k, v in metadata.items():
+            #     print(f"{k}: {v}")
 
             # testing integrity
             # [1] calibration
--- Integrity_Audio_Files/test_integrity.py
+++ Integrity_Audio_Files/test_integrity.py
@@ -30,11 +30,6 @@ def test_name_calibration(metadata: dict, logger, calibration_file='calibration_
     calibration_dict = {k.upper() : v for k, v in config['CalibrationConstants'].items()}
     file_calibration = calibration_dict.get(audiomoth_name, None)
     
-    if audiomoth_name in calibration_dict:
-        logger.debug(f"For {audiomoth_name}, the calibration constants is {calibration_dict[audiomoth_name]}")
-    else:
-        logger.warning(f"For {audiomoth_name}, there is no calibration constant")
-    
     return filename, audiomoth_name, file_calibration
 
 
@@ -42,11 +37,6 @@ def test_channels(metadata: dict, logger):
     """Get the number of channels from the metadata."""
     channels = metadata["channels"]
     channels = int(channels)
-    
-    if channels == CHANNELS:
-        logger.debug(F"Number of channels is properly set to {channels}")
-    else:
-        logger.debug(f"Number of channels set to {channels}, not to {CHANNELS}")
 
     return channels
 
@@ -54,38 +44,26 @@ def test_channels(metadata: dict, logger):
 def test_sample_rate(metadata: dict, logger):
     """Get the sample rate from the metadata."""
     sample_rate = metadata["sample_rate"]
-    
-    if sample_rate == SAMPLE_RATE:
-        logger.debug(f"Sample rate is properly set to {sample_rate}")
-    else:
-        logger.warning(f"Sample rate is not set to {SAMPLE_RATE}, but to {sample_rate}")
 
     return sample_rate
 
 
 def test_time_zone(metadata: dict, logger):
-    """Get the time zone from the metadata."""
+    """Get the time zone from the metadata.
+    desired time zone (UTC +1)
+    """
     comment = get_comment_section(metadata)
     time_zone_metadata = comment[4]
-    time_zone_madrid = "(UTC +1)"
 
-    if comment[4] == time_zone_madrid:
-        logger.debug(f"Time zone is properly set to {time_zone_metadata}")
-    else:
-        logger.warning(f"Time zone is set to {time_zone_metadata}, not to {time_zone_madrid}") 
-    
     return time_zone_metadata
 
 
 def test_batery_status(metadata: dict, logger):
-    """Get the battery status from the metadata."""
+    """Get the battery status from the metadata.
+     3.5 V; battery voltage too low to reliably record.
+     """
     comment = get_comment_section(metadata)
     battery_voltage = float(comment[14][:-1])
-
-    if battery_voltage < 5.0 and battery_voltage > 0.0:
-        logger.debug(f"Battery status is okay: {battery_voltage}V")
-    else:
-        logger.warning(f"Battery status is not ok {battery_voltage}V")
 
     return battery_voltage
 
@@ -93,35 +71,22 @@ def test_gain(metadata: dict, logger):
     """Get the gain from the metadata."""
     comment = get_comment_section(metadata)
     gain = comment[9]
-    if gain == GAIN:
-        logger.debug(f"Gain is properly set to {gain}")
-    else:
-        logger.warning(f"Gain is not set to {GAIN}, but to {gain}")
 
     return gain
 
 def test_recording_duration(metadata: dict, logger):
     duration = metadata["duration"]
     duration = round(float(duration), 2)
-    # round duration
-
-    if duration == RECORDING_DURATION:
-        logger.debug(f"Recording duration is properly set to {duration}s")
-    else:
-        logger.warning(f"Recording duration is not properly set to {RECORDING_DURATION}s, but to {duration}s")
 
     return duration
 
 def test_temperature(metadata: dict, logger):
-    """Get the temperature from the metadata"""
+    """Get the temperature from the metadata
+    there is no temperature range in the configuration guide
+    """
     comment = get_comment_section(metadata)
     temperature = comment[-1].split("C")[0]
     temperature = float(temperature)
-
-    if temperature > 0.0 and temperature < 50.0:
-        logger.debug(f"Temperature {temperature} within range")
-    else:
-        logger.warning(f"Temperature is  {temperature} within range")
 
     return temperature
 
@@ -130,6 +95,4 @@ def test_timestamp(metadata: dict, logger):
     comment = get_comment_section(metadata)
     timestamp = comment[2:4]
     
-    logger.debug(f"Timestamp is {timestamp}")
-
     return timestamp

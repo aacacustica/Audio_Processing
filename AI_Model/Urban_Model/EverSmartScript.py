956901b2cd289a037cb6259ed63cf2f9d55bb0a3
Santiago Campo Jurado <id00706137@usal.es>
2024-03-05 16:07:57 +0100

    Add logging statements and improve datetime extraction in EverSmartScript.py

--- AI_Model/Urban_Model/EverSmartScript.py
+++ AI_Model/Urban_Model/EverSmartScript.py
@@ -6,6 +6,8 @@ import datetime
 
 import soundfile as sf
 import argparse
+import re
+
 
 import params
 import yamnet as yamnet_model
@@ -22,23 +24,24 @@ logging.basicConfig(level=logging.INFO,
 
 
 def get_predictions(audio_files:list, fs_model:float, w_time:int, n_predictions:int):
-    params.PATCH_HOP_SECONDS = 1 # 1 Hz frame rate.
+    params.PATCH_HOP_SECONDS = 1 # 1 Hz frame rate
     params.SAMPLE_RATE = int(fs_model) 
+    logging.info(f"Model confifured with {params.PATCH_HOP_SECONDS} Hz frame rate")
     logging.info(f"Model configured with fs= {int(fs_model)}")
     
     yamnet = yamnet_model.yamnet_frames_model(params)
     yamnet.load_weights('yamnet.h5')
     class_names = yamnet_model.class_names('yamnet_class_map.csv')
-
+    logging.info(f"YAMNet model loaded with {len(class_names)} classes")
+    
     classes = [] 
     probabilities = [] 
     datetimes = []
     files = []
     spectrograms = []
-
+    
     for file in tqdm(audio_files):
         logging.info(f"Processing audio file  -->  {file}")
-        
         try:
             wav_data, sr = sf.read(os.path.join(audio_path, file), dtype=np.int16)
         except Exception as e:
@@ -49,12 +52,27 @@ def get_predictions(audio_files:list, fs_model:float, w_time:int, n_predictions:
         w_size = int(w_time * sr)
         # w_size = int(w_time * 60 * sr)
         
-        start_datetime = datetime.datetime.strptime(file.split('.')[0], '%Y%m%d_%H%M%S')
+        try:
+            match = re.search(r'(\d{8}_\d{6})', file.split('.')[0])
+            if match:
+                date_str = match.group(1)  # Extract the datetime part
+                start_datetime = datetime.datetime.strptime(date_str, '%Y%m%d_%H%M%S')
+            else:
+                logging.info(f"Non-standard file name, skipping datetime extraction: {file}")
+                start_datetime = datetime.datetime.now()
+        except ValueError as e:
+            logging.error(f"Error parsing datetime from file name {file}: {e}")
+            start_datetime = datetime.datetime.now()  # Use current time as a fallback
+        
+        # start the log
+        inference_start_time = datetime.datetime.now()
 
         if len(waveform) > w_size:
             for count, fstart in enumerate(range(0, len(waveform) - w_size + 1, w_size)):
+                # get the predictions
                 scores, spectrogram = yamnet.predict(np.reshape(waveform[fstart:fstart+w_size], [1, -1]), steps=1)
                 prediction = np.mean(scores, axis=0)
+                # add spectrogram
                 spectrograms.append(spectrogram)
 
                 # get the top n predictions
@@ -67,14 +85,20 @@ def get_predictions(audio_files:list, fs_model:float, w_time:int, n_predictions:
                 # append to the lists
                 classes.append(clip_classes)
                 probabilities.append(prob_classes)
-                
+                        
                 # set time column
                 window_datetime = start_datetime + datetime.timedelta(seconds=w_time * count)
                 datetimes.append(window_datetime)
 
+                # append file
+                files.append(file) 
+                
+                inference_end_time = datetime.datetime.now()
+                inference_duration = inference_end_time - inference_start_time
+                logging.info(f"Inference for {file} took {inference_duration.total_seconds()} seconds.")
+
         else:
             logging.warning("Audio shorter than analysis window")
-
 
     # save the results in a dataframe
     data_dict = {'file': files, 
@@ -82,11 +106,13 @@ def get_predictions(audio_files:list, fs_model:float, w_time:int, n_predictions:
                  'classes_original': classes, 
                  'probabilities_original': probabilities,
                 #  'spectrograms': spectrograms
+                
                  } 
 
     # sort by datetime in ascending order
     df = pd.DataFrame(data_dict)
-    df_sorted = df.sort_values(by='datetime')
+    # df_sorted = df.sort_values(by='datetime')
+    df_sorted = df.sort_values(by='file')
 
     return df_sorted
 
@@ -102,19 +128,20 @@ def argument_parser():
 if __name__ == "__main__":
     """
     example of use:
-            python urban_model.py -p /home/usuario/audios -a "audios_1" -w 14.99 -n 3 -r /home/usuario/resultados
+            python EverSmartScript.py -p /home/usuario/audios -a "audios_1" -w 14.99 -n 3 -r /home/usuario/resultados
     """
     args = argument_parser()
-    
     # set path to audio files
     audio_path = args.path
     # set analysis window size in minutes
     analysis_window_time = args.window
     # set number of predictions
     n_predictions = args.n_predictions
+    
     # set results folder
     if args.result_folder:
         results_dir = args.result_folder
+        results_dir = os.path.join(results_dir, 'YAMet_results')
         if not os.path.exists(results_dir):
             os.makedirs(results_dir)
         make_predicciones = results_dir
@@ -155,7 +182,6 @@ if __name__ == "__main__":
         data_df = get_predictions(audio_files=valid_audio_files,
                                   fs_model=fs_model,
                                   w_time=analysis_window_time,
-                                #   w_time=1/60,
                                   n_predictions=n_predictions)
     except Exception as e:
         raise Exception(f"Error generating predictions: {e}")
